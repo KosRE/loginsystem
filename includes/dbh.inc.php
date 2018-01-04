301e1a1b2aeb042362581e310fda3d030a6c44be
RE Kosch <34550872+KosRE@users.noreply.github.com>
2018-01-03 22:06:04 -0600

    Initial commit

---
+++ includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+/*DB connector*/
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "loginsystem";
+
+$con = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName );
